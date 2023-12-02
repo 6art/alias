b88bf244ddaa44786963ba09197486f3d58aa993
clashingone <119949084+clashingone@users.noreply.github.com>
2023-12-02 21:53:23 +0800

    Update setup_aliases.sh

--- setup_aliases.sh
+++ setup_aliases.sh
@@ -3,33 +3,26 @@
 # 添加一个新的别名函数
 # 这个函数接受两个参数:别名和对应的命令
 add_alias() {
-  local alias_name=$1
-  local command=$2
+    local alias_name=$1
+    local command=$2
 
-  # 检查是否已存在
-  if ! grep -q "alias $alias_name=" /etc/bash.bashrc; then 
-    echo "alias $alias_name='$command'" >> /etc/bash.bashrc
-    echo "别名 '$alias_name' 已添加。"  
-  else
-    echo "别名 '$alias_name' 已存在,无需重复添加。"
-  fi
+    # 检查是否已存在
+    if ! grep -q "alias $alias_name=" ~/.bashrc; then
+        echo "alias $alias_name='$command'" >> ~/.bashrc
+        echo "别名 '$alias_name' 已添加。"
+    else
+        echo "别名 '$alias_name' 已存在,无需重复添加。"
+    fi
 }
 
-# 使用 add_alias 函数添加你的别名 
+# 使用 add_alias 函数添加你的别名
 # 你可以根据需要添加更多别名
 # 调用格式:add_alias "别名" "命令"
-
 # 示例:添加别名 lion
-add_alias "lion" "curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh"  
+add_alias "lion" "curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh"
 
-# 重复上述模式以添加更多别名 
+# 重复上述模式以添加更多别名
 # 比如: add_alias "tiger" "your_command"
 
-# 添加函数定义到公共配置文件
-echo "" >> /etc/bash.bashrc
-echo "add_alias() {" >> /etc/bash.bashrc 
-echo " ..." >> /etc/bash.bashrc
-
 # 重新加载以立即应用
-source /etc/bash.bashrc
-exec bash
+source ~/.bashrc
