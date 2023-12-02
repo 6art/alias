97d1b621db3e6c260d090bbcbf46f1447259185f
clashingone <119949084+clashingone@users.noreply.github.com>
2023-12-02 22:46:45 +0800

    Update setup_aliases.sh

--- setup_aliases.sh
+++ setup_aliases.sh
@@ -17,35 +17,39 @@ add_alias() {
 
 # 显示所有别名及其对应的命令
 show_aliases() {
-    echo "序号 别名 命令"
+    echo -e "序号\t别名\t命令"
     grep -oP '^alias \K.*' /etc/bash.bashrc | cat -n
+    echo "按任意键返回主菜单..."
+    read
 }
 
 # 显示菜单并获取用户的选择
-echo "请选择一个操作："
-echo "1. 添加所有收藏脚本的别名"
-echo "2. 查看所有收藏脚本"
-read -p "请输入你的选择（1 或 2）：" choice
+while true; do
+    echo "请选择一个操作："
+    echo "1. 添加所有收藏脚本的别名"
+    echo "2. 查看所有收藏脚本"
+    read -p "请输入你的选择（1 或 2）：" choice
 
-# 根据用户的选择执行相应的操作
-case "$choice" in
-    1)
-        # 使用 add_alias 函数添加你的别名
-        # 你可以根据需要添加更多别名
-        # 调用格式:add_alias "别名" "命令"
-        # 示例:添加别名 lion
-        add_alias "lion" "curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh"
+    # 根据用户的选择执行相应的操作
+    case "$choice" in
+        1)
+            # 使用 add_alias 函数添加你的别名
+            # 你可以根据需要添加更多别名
+            # 调用格式:add_alias "别名" "命令"
+            # 示例:添加别名 lion
+            add_alias "lion" "curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh"
 
-        # 重复上述模式以添加更多别名
-        # 比如: add_alias "tiger" "your_command"
+            # 重复上述模式以添加更多别名
+            # 比如: add_alias "tiger" "your_command"
 
-        # 重新加载以立即应用
-        echo "source /etc/bash.bashrc" >> /etc/bash.bashrc
-        ;;
-    2)
-        show_aliases
-        ;;
-    *)
-        echo "无效的选择。"
-        ;;
-esac
+            # 重新加载以立即应用
+            echo "source /etc/bash.bashrc" >> /etc/bash.bashrc
+            ;;
+        2)
+            show_aliases
+            ;;
+        *)
+            echo "无效的选择。"
+            ;;
+    esac
+done
