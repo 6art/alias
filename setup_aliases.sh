bf9765453dda2d859c13efddd7114d1fcce007f9
clashingone <119949084+clashingone@users.noreply.github.com>
2023-12-02 22:13:37 +0800

    Update setup_aliases.sh
    对所有用户生效

--- setup_aliases.sh
+++ setup_aliases.sh
@@ -7,11 +7,11 @@ add_alias() {
     local command=$2
 
     # 检查是否已存在
-    if ! grep -q "alias $alias_name=" ~/.bashrc; then
-        echo "alias $alias_name='$command'" >> ~/.bashrc
-        echo "别名 '$alias_name' 已添加。"
+    if ! grep -q "alias $alias_name=" /etc/bash.bashrc; then
+        echo "alias $alias_name='$command'" >> /etc/bash.bashrc
+        echo "别名 '$alias_name' 已添加。你现在可以使用这个别名来执行你的命令了！"
     else
-        echo "别名 '$alias_name' 已存在,无需重复添加。"
+        echo "别名 '$alias_name' 已存在，无需重复添加。你可以直接使用这个别名来执行你的命令！"
     fi
 }
 
@@ -25,4 +25,4 @@ add_alias "lion" "curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main
 # 比如: add_alias "tiger" "your_command"
 
 # 重新加载以立即应用
-source ~/.bashrc
+source /etc/bash.bashrc
